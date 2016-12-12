0f78fe394263395121194714d95adcfb0870bfd4
sagiters19 <sagiters19@hotmail.com>
2016-12-12 21:39:17 +0800

    Completed Part 11

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -8,22 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var videos = [Videos]()
 
     @IBOutlet var displayLabel: UILabel!
     
+    @IBOutlet var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self;
+        tableView.delegate = self;
         
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.reachabilityStatusChanged), name: NSNotification.Name(rawValue: "ReachStatusChanged"), object: nil);
         reachabilityStatusChanged();
         
         // Call API
         let api = APIManager();
-        api.loadData(urlString: "https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json", completion: didLoadData)
+        api.loadData(urlString: "https://itunes.apple.com/us/rss/topmusicvideos/limit=50/json", completion: didLoadData)
         
     }
 
@@ -40,6 +45,8 @@ class ViewController: UIViewController {
         for (index, item) in videos.enumerated() {
             print("\(index) name = \(item.vName)");
         }
+        
+        tableView.reloadData();
         
 //        for i in 0..<videos.count {
 //            let video = videos[i];
@@ -69,6 +76,32 @@ class ViewController: UIViewController {
     deinit {
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: "ReachStatusChanged"), object: nil);
     }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath);
+        
+        let video = videos[indexPath.row];
+        
+        cell.textLabel?.text = ("\(indexPath.row + 1)");
+        
+        cell.detailTextLabel?.text = video.vName
+        
+        return cell;
+        
+    }
+    
+    
+    
 
 }
 
