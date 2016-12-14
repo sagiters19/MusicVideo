658102629df822b0e8f417be53c5641247699314
sagiters19 <sagiters19@hotmail.com>
2016-12-14 22:38:53 +0800

    Completed Part 28

--- MusicVideo/SettingTVC.swift
+++ MusicVideo/SettingTVC.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class SettingTVC: UITableViewController {
+class SettingTVC: UITableViewController, MFMailComposeViewControllerDelegate {
 
     
     @IBOutlet var aboutDisplay: UILabel!
@@ -74,6 +75,67 @@ class SettingTVC: UITableViewController {
         numberOfVideosDisplay.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline);
         dragTheSliderDisplay.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.footnote);
         
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.section == 0 && indexPath.row == 1 {
+            
+            let mailComposeViewController = configureMail();
+            
+            if MFMailComposeViewController.canSendMail() {
+                self.present(mailComposeViewController, animated: true, completion: nil);
+            } else {
+                mailAlert();
+            }
+            
+            tableView.deselectRow(at: indexPath, animated: true);
+            
+        }
+        
+    }
+    
+    func configureMail() -> MFMailComposeViewController {
+        
+        let mailComposeVC = MFMailComposeViewController();
+        mailComposeVC.mailComposeDelegate = self;
+        mailComposeVC.setToRecipients(["sagiters19@gmail.com"]);
+        mailComposeVC.setSubject("Music Video App Feedback");
+        mailComposeVC.setMessageBody("Hi Jack,\n\nI would like to share the following feedback...\n", isHTML: false);
+        return mailComposeVC;
+        
+    }
+    
+    func mailAlert() {
+        
+        let alertController: UIAlertController = UIAlertController(title: "Alert", message: "No e-Mail Account setup for Phone", preferredStyle: .alert);
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+            
+        }
+        
+        alertController.addAction(okAction);
+        
+        self.present(alertController, animated: true, completion: nil);
+        
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        switch result.rawValue {
+        case MFMailComposeResult.cancelled.rawValue:
+            print("Mail cancelled");
+        case MFMailComposeResult.saved.rawValue:
+            print("Mail saved");
+        case MFMailComposeResult.sent.rawValue:
+            print("Mail sent");
+        case MFMailComposeResult.failed.rawValue:
+            print("Mail Failed");
+        default:
+            print("Unknown Issue");
+        }
+        self.dismiss(animated: true, completion: nil);
         
     }
     
