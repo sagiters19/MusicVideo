5d904a432d367a5ccdbe6e2dec5845b23c16455a
sagiters19 <sagiters19@hotmail.com>
2016-12-14 21:26:15 +0800

    Completed Part 25

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class MusicVideoDetailVC: UIViewController {
 
@@ -39,6 +41,21 @@ class MusicVideoDetailVC: UIViewController {
         }
         
     }
-
+    
+    @IBAction func playVideo(_ sender: UIBarButtonItem) {
+        
+        let url = NSURL(string: videos.vVideoUrl)
+        
+        let player = AVPlayer(url: url as! URL);
+        
+        let playerViewController = AVPlayerViewController();
+        
+        playerViewController.player = player;
+        
+        self.present(playerViewController, animated: true) { 
+            playerViewController.player?.play();
+        }
+        
+    }
 
 }
